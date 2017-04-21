5e43fc823ce8c465c03724844a4a5de906f39420
server <cephei.1313@gmail.com>
2017-04-21 17:39:01 -0300

    updated config files for velocity app

--- uoctWebPlatform/settings.py
+++ uoctWebPlatform/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'velocity',
 ]
 
 MIDDLEWARE = [
@@ -113,5 +114,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/uoct/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- uoctWebPlatform/urls.py
+++ uoctWebPlatform/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="base.html")),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='velocity/map', permanent=True), name="index"),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^velocity/', include('velocity.urls')),
 ]
