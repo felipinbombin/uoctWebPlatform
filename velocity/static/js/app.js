0651edc3c4af03f2171c89cbcb6c2e796e79b9e3
Felipe Hernández <cephei.1313@gmail.com>
2017-07-12 18:04:05 -0400

    refactor

--- velocity/static/js/app.js
+++ velocity/static/js/app.js
@@ -1,5 +1,4 @@
-let App = (function(aa){
-  
+let App = (function(){
   let Element = function(layer, color, group, name) {
     this.layer = layer;
     this.color = color;
@@ -35,20 +34,20 @@ let App = (function(aa){
     }).setView(beauchefLocation, 13);
 
     // mapbox map styles
-    let mapboxURL = 'https://api.mapbox.com/styles/v1/';
-    let token = 'pk.eyJ1IjoidHJhbnNhcHAiLCJhIjoiY2lzbjl6MDQzMDRkNzJxbXhyZWZ1aTlocCJ9.-xsBhulirrT0nMom_Ay9Og';
-    let blackStyle= mapboxURL + 'mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=' + token;
-    let grayStyle= mapboxURL + 'mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=' + token;
-    let stopStyle= mapboxURL + 'transapp/cj3a8ksvx000t2sqstwxzbc2f/tiles/256/{z}/{x}/{y}?access_token=' + token;
-    let attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                    'Imagery © <a href="http://mapbox.com">Mapbox</a>';
- 
+    let mapboxURL = "https://api.mapbox.com/styles/v1/";
+    let token = "pk.eyJ1IjoidHJhbnNhcHAiLCJhIjoiY2lzbjl6MDQzMDRkNzJxbXhyZWZ1aTlocCJ9.-xsBhulirrT0nMom_Ay9Og";
+    let blackStyle= mapboxURL + "mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=" + token;
+    let grayStyle= mapboxURL + "mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=" + token;
+    let stopStyle= mapboxURL + "transapp/cj3a8ksvx000t2sqstwxzbc2f/tiles/256/{z}/{x}/{y}?access_token=" + token;
+    let attribution = "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, " +
+                    "Imagery © <a href='http://mapbox.com'>Mapbox</a>";
+
     // map layers
     let grayBaseLayerWithStops = L.tileLayer(stopStyle, {attribution: attribution});
     let grayBaseLayer = L.tileLayer(grayStyle, {attribution: attribution});
     // default map
     grayBaseLayer.addTo(map);
-  
+
     let modalContent =
  '<div class="col-md-12 col-sm-12 col-xs-12">' +
    '<div class="x_panel">' +
@@ -83,81 +82,82 @@ let App = (function(aa){
      '</div>'+
    '</div>'+
   '</div>';
-  
+
     let win = L.control.window(map, {
       content: modalContent,
       closeButton: true,
       modal: true,
-      position: 'top',
+      position: "top"
     });
     // activate tabs
-    $('.nav-tabs a').click(function(e){
+    $(".nav-tabs a").click(function(e){
       e.preventDefault();
-      $(this).tab('show');
+      $(this).tab("show");
     });
     // set all and empty checkbox
-    $('input[type="radio"].flat').iCheck({
+    $("input[type='radio'].flat").iCheck({
       labelHover: false,
       cursor: true,
-      checkboxClass: 'icheckbox_flat-green',
-      radioClass: 'iradio_flat-green',
+      checkboxClass: "icheckbox_flat-green",
+      radioClass: "iradio_flat-green"
     });
-    $('input[type=radio][name=filter]').on('ifChanged', function (event) {
-      if ($("input[name='filter']:checked").attr('id') == 'chkAll') {
-        $("input[type=checkbox]").iCheck('check');
+    $("input[type=radio][name=filter]").on("ifChanged", function () {
+      let id = $("input[name='filter']:checked").attr("id");
+      if (id === "chkAll") {
+        $("input[type=checkbox]").iCheck("check");
       }
-      if ($("input[name='filter']:checked").attr('id') == 'chkEmpty') {
-        $("input[type=checkbox]").iCheck('uncheck');
+      if (id === "chkEmpty") {
+        $("input[type=checkbox]").iCheck("uncheck");
       }
     });
 
     // DEFINE HOUR CONTROL
     let hourControl = L.control({position: 'topleft'});
     hourControl.onAdd = function (map) {
-      let div = L.DomUtil.create('div', 'info hour');
+      let div = L.DomUtil.create("div", "info hour");
       div.innerHTML = '<i class="fa fa-clock-o fa-lg" aria-hdden="true"></i> <span id="hourInfo"></span>';
       return div;
-    };  
+    };
     hourControl.addTo(map);
- 
+
     // DEFINE DAY TYPE CONTROL
     let dayTypeControl = L.control({position: 'topleft'});
     dayTypeControl.onAdd = function (map) {
       let div = L.DomUtil.create('div', 'info day');
       div.innerHTML = '<i class="fa fa-calendar-o fa-lg" aria-hdden="true"></i> <span id="dayType"></span>';
       return div;
-    }; 
+    };
     dayTypeControl.addTo(map);
- 
+
     let filterControl = L.easyButton({
-      position: 'topright',
+      position: "topright",
       states: [{
-        stateName: 'filter',
-        icon: '<i id="filterButton" class="fa fa-filter fa-lg"></i>',
+        stateName: "filter",
+        icon: "<i id='filterButton' class='fa fa-filter fa-lg'></i>",
         onClick: function(btn, map) {
           win.show();
         }
       }]
     }).addTo(map);
-  
+
     // DEFINE SWITCH MAP
     let switchMapControl = L.easyButton({
-      position: 'topright',
+      position: "topright",
       states: [{
-        stateName: 'map',
-        icon: '<i id="switchMap" class="fa fa-map-signs fa-lg"></i>',
+        stateName: "map",
+        icon: "<i id='switchMap' class='fa fa-map-signs fa-lg'></i>",
         onClick: function(btn, map) {
           map.removeLayer(grayBaseLayer);
           map.addLayer(grayBaseLayerWithStops);
-          btn.state('mapWithStops');
+          btn.state("mapWithStops");
         }
       },{
-        stateName: 'mapWithStops',
+        stateName: "mapWithStops",
         icon: '<i id="switchMap" class="fa fa-map fa-lg"></i>',
         onClick: function(btn, map) {
           map.removeLayer(grayBaseLayerWithStops);
           map.addLayer(grayBaseLayer);
-          btn.state('map');
+          btn.state("map");
         }
       }]
     }).addTo(map);
@@ -167,18 +167,17 @@ let App = (function(aa){
     });
     map.invalidateSize();
 
-    let wrapper = {
+    return {
       map: map,
       controls: {
-        'switchMap': switchMapControl,
-        'filter': filterControl, 
-        'hourInfo': hourControl, 
-        'dayTypeInfo': dayTypeControl
+        "switchMap": switchMapControl,
+        "filter": filterControl,
+        "hourInfo": hourControl,
+        "dayTypeInfo": dayTypeControl
       }
     };
-    return wrapper;
-  };
-  
+  }
+
   function App(getMetricFunc, getColorFunc, getColorPositionFunc, getBubbleInfo){
     let _wrapper = getMap();
     let _map = _wrapper.map;
@@ -190,9 +189,9 @@ let App = (function(aa){
     let _getBubbleInfo = getBubbleInfo;
     let _makeBounds = true;
 
-	this.setMetric = function(getMetricFunc){
+    this.setMetric = function(getMetricFunc){
       _getMetric = getMetricFunc;
-	};
+    };
 
     this.setMapControlVisibility = function(controlName, val){
       let control = _mapControls[controlName];
@@ -200,7 +199,7 @@ let App = (function(aa){
         if (val){
           _map.addControl(control);
         }
-    }
+    };
     let _categorizeSection = function(layer, color, group, name, id) {
       // if id is present in data
       if(_dataSource.hasOwnProperty(id)){
@@ -211,10 +210,10 @@ let App = (function(aa){
         el.updateLayer(layer, _map);
         return;
       }
-  
+
       _dataSource[id] = new Element(layer, color, group, name);
-    }
-  
+    };
+
     // Add control to map
     this.addMapControl = function(legendControl){
       legendControl.addTo(_map);
@@ -228,33 +227,33 @@ let App = (function(aa){
      * CREATE OVERLAYS
      */
     this.generateOverlays = function(streetName, sectionName, section) {
-  
+
       let latLngs = [];
       let id = section.id;
       let metrics = _getMetric(section);
       let group = section.group;
       let points = section.points;
-  
+
       // order points by distOnRoute
       points.sort(function(a, b) {
         return a.distOnRoute - b.distOnRoute;
       });
-   
+
       // populate positions
       $.each(points, function(i2, point) {
         latLngs.push([point.latitude, point.longitude]);
       });
-  
+
       let line = L.polyline(latLngs, {
         color: _getColor(metrics),
         //weight: 6,
       });
-  
+
       let patternsOpts = [
       // defines a pattern of 10px-wide dashes, repeated every 20px on the line
       {offset: 10,
         endOffset: 5,
-        repeat: '5%',
+        repeat: "5%",
         symbol: L.Symbol.longArrowHead({
           rotate: true,
           pixelSize: 5,
@@ -267,49 +266,49 @@ let App = (function(aa){
           }
         })
       }
-      ]
+      ];
       let decorator = L.polylineDecorator(line, {patterns: patternsOpts});
-   
+
       //********************************************************/
       // set bubble
       let message = _getBubbleInfo(metrics, streetName, section);
       decorator.bindPopup(message);
       line.bindPopup(message);
-  
+
       //********************************************************/
       // animation
       let arrowOffset = 0;
       patternsOpts[0].repeat = 0;
       let anim = window.setInterval(function() {
-        patternsOpts[0].offset = arrowOffset + '%';
+        patternsOpts[0].offset = arrowOffset + "%";
         decorator.setPatterns(patternsOpts);
         if(arrowOffset > 100)
           arrowOffset = 0;
         arrowOffset = arrowOffset + 1;
       }, 50);
       //********************************************************/
-   
+
       // create overlay
       let overlay = L.featureGroup([]);
       overlay.addLayer(line);
       overlay.addLayer(decorator);
-   
+
       _categorizeSection(overlay, _getColor(metrics), group, streetName, id);
-   
+
       console.log("Map updated with polyline decorator");
     };
-  
+
     this.updateMap = function(hour, dayType) {
-  
+
       // update map info
-      $('#hourInfo').text(hour);
-      $('#dayType').text(dayType);
-  
+      $("#hourInfo").text(hour);
+      $("#dayType").text(dayType);
+
       // update filter
       this.updateFilterWindow(_getColorPosition);
-  
+
       let bounds = null;
-  
+
       // add layers to map and control
       $.each(_dataSource, function(index, element){
         if (element.visible){
@@ -321,13 +320,13 @@ let App = (function(aa){
           }
         }
       });
-  
+
       if(bounds !== null && _makeBounds){
         _map.fitBounds(bounds);
         _makeBounds = false;
       }
     };
-   
+
     /**
     * DEFINE FILTER
     */
@@ -342,84 +341,84 @@ let App = (function(aa){
         x[y] = 1;
         return x;
       }, {});
-  
+
       let orderedList = [];
       $.each(dict, function(attrValue, frecuency){
         orderedList.push({attrValue:attrValue, frecuency:frecuency});
       });
       orderedList.sort(sortFunction);
       return orderedList;
-    }
-  
+    };
+
     let _generateGroupList = function(divId, attrName, sortFunction, getLabel) {
       let orderedList = _getAttributeUniqueList(attrName, sortFunction);
       $.each(orderedList, function(index, item){
         let attrValue = item.attrValue;
         let frecuency = item.frecuency;
         let label = getLabel(attrValue, frecuency);
-  
+
         let checkbox = null;
-        let checkboxId = attrValue.replace(/\.|-| |#/g, '');
-        let checkboxExists = $('#'+checkboxId).length ? true:false;
+        let checkboxId = attrValue.replace(/\.|-| |#/g, "");
+        let checkboxExists = $("#" + checkboxId).length ? true:false;
         if (checkboxExists){
-          checkbox = $('#'+checkboxId);
+          checkbox = $("#" + checkboxId);
           checkbox.parent().next().html(label);
-          checkbox.off('ifChecked');
-          checkbox.off('ifUnchecked');
+          checkbox.off("ifChecked");
+          checkbox.off("ifUnchecked");
         } else {
           let html = $("<div class='checkbox'><label>" + label + "</label></div>");
           checkbox = $("<input id='"+checkboxId+"' type='checkbox' class='flat' checked='checked' />");
           html.prepend(checkbox);
-          $('#' + divId).append(html);
+          $("#" + divId).append(html);
           checkbox.iCheck({
             labelHover: false,
             cursor: true,
-            checkboxClass: 'icheckbox_flat-green',
-            radioClass: 'iradio_flat-green',
+            checkboxClass: "icheckbox_flat-green",
+            radioClass: "iradio_flat-green",
           });
         }
-        checkbox.on('ifChecked', function(event){
+        checkbox.on("ifChecked", function(){
           $.each(_dataSource, function(index, element){
-            if (attrValue == element[attrName]) {
+            if (attrValue === element[attrName]) {
               element.addToMap(_map);
             }
           });
         });
-        checkbox.on('ifUnchecked', function(event){
+        checkbox.on("ifUnchecked", function(){
           $.each(_dataSource, function(index, element){
-            if (attrValue == element[attrName]) {
+            if (attrValue === element[attrName]) {
               element.removeFromMap();
             }
           });
         });
       });
     };
-  
+
     this.updateFilterWindow = function(){
       // streets
-      _generateGroupList('listContent', 'name', function(a, b){
+      _generateGroupList("listContent", "name", function(a, b){
         return a.attrValue.localeCompare(b.attrValue);
       }, function(name, frecuency){
         return " " + name + " (" + frecuency + " tramos)";
       });
-  
+
       // groups
-      _generateGroupList('groupContent', 'group', function(a, b){
+      _generateGroupList("groupContent", "group", function(a, b){
         return a.attrValue.localeCompare(b.attrValue);
       }, function(group, frecuency){
         return " " + group + " (" + frecuency + " tramos)";
       });
-  
+
       // colors
-      _generateGroupList('colorContent', 'color', function(a, b){
+      _generateGroupList("colorContent", "color", function(a, b){
         return _getColorPosition(a.attrValue) - _getColorPosition(b.attrValue);
       }, function(color, frecuency){
         return " (" + frecuency + " tramos)<i class='filterColor' style='background:" + color + "'></i>";
       });
-  
-      console.log('checkboxs info updated');
+
+      console.log("checkboxs info updated");
     };
   }
-  
+
   return App;
 })();
