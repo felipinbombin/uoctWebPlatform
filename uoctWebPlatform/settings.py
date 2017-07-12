d3e3c3a9dc06791101c43a3e9afc7f922f82a311
Felipe Hernández <cephei.1313@gmail.com>
2017-07-12 17:31:19 -0400

    updated polyline decorator plugin

--- uoctWebPlatform/settings.py
+++ uoctWebPlatform/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secretKey.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'200.9.100.91', '172.17.77.240']
+ALLOWED_HOSTS = [u'200.9.100.91', u'172.17.77.240', u'172.17.57.156']
 
 
 # Application definition
